91620cb1c827ac44dd0856aa36a6ab641d80720c
王爽陟 <sharrie@wangshuangzhideMacBook-Pro.local>
2018-06-22 22:05:09 +0800

    初步完成

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
 var express = require("express");
 var app = express();
-app.use("/",express.static(__dirname + "/home"));
-app.use("/proj",express.static(__dirname + "/proj"));
+var path = require("path")
+var fs = require("fs")
+
+// 主页设置
+app.use(express.static('home'));
+
+//获取项目列表
+app.get('/getlist',(req, res, next) => {
+  var path2 = path.resolve(__dirname, './proj');
+  fs.readdir(path2, function (err, files) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    var count = files.length;
+    var results = [];
+    files.forEach(function (filename) {
+      if (!filename.match(/\./)) {
+        results.push(filename);
+      } 
+    });
+    res.send({res:results})
+    next()
+  })
+})
+
+//项目页面设置
+app.get("/proj/*",(req, res, next) => {
+  var targetProj = req._parsedOriginalUrl.pathname.match(/proj(\S*)\//)[1]
+  res.sendFile( __dirname + req._parsedOriginalUrl.pathname );
+});
+
+
 app.listen(3000);
